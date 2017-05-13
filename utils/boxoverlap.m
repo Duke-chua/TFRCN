be13a2fabb8dc022bb1aaa75404dc01671ac44d8
xuzhewei <xzhewei@gmail.com>
2017-05-13 22:32:18 +0800

    add comments

--- utils/boxoverlap.m
+++ utils/boxoverlap.m
@@ -20,21 +20,21 @@ function o = boxoverlap(a, b)
 
 o = cell(1, size(b, 1));
 for i = 1:size(b, 1)
-    x1 = max(a(:,1), b(i,1));
-    y1 = max(a(:,2), b(i,2));
-    x2 = min(a(:,3), b(i,3));
-    y2 = min(a(:,4), b(i,4));
+    x1 = max(a(:,1), b(i,1)); %左上选坐标大的
+    y1 = max(a(:,2), b(i,2)); %左上选坐标大的
+    x2 = min(a(:,3), b(i,3)); %右下选坐标小的
+    y2 = min(a(:,4), b(i,4)); %右下选坐标小的
 
-    w = x2-x1+1;
-    h = y2-y1+1;
+    w = x2-x1+1; %交集部分宽
+    h = y2-y1+1; %交集部分高
     inter = w.*h;
     aarea = (a(:,3)-a(:,1)+1) .* (a(:,4)-a(:,2)+1);
     barea = (b(i,3)-b(i,1)+1) * (b(i,4)-b(i,2)+1);
-    % intersection over union overlap IoU
-    o{i} = inter ./ (aarea+barea-inter);
+    % intersection over union overlap
+    o{i} = inter ./ (aarea+barea-inter); %交/并
     % set invalid entries to 0 overlap
     o{i}(w <= 0) = 0;
     o{i}(h <= 0) = 0;
 end
 
-o = cell2mat(o);
+o = cell2mat(o); %cell转mat
--- utils/prep_im_for_blob.m
+++ utils/prep_im_for_blob.m
@@ -1,11 +1,12 @@
 function [im, im_scale] = prep_im_for_blob(im, im_means, target_size, max_size)
-    im = single(im);
+    im = single(im); % im 转换为 single 单精度
     
     if ~isa(im, 'gpuArray')
         try
-            im = bsxfun(@minus, im, im_means);
-        catch
-            im_means = imresize(im_means, [size(im, 1), size(im, 2)], 'bilinear', 'antialiasing', false);    
+            im = bsxfun(@minus, im, im_means); % im 和 im_means对应元素相减
+        catch % im 和 im_means 的尺寸不匹配
+            % im_means 按照 im 的尺寸进行resize，进行双线性差值、抗锯齿
+            im_means = imresize(im_means, [size(im, 1), size(im, 2)], 'bilinear', 'antialiasing', false);
             im = bsxfun(@minus, im, im_means);
         end
         im_scale = prep_im_for_blob_size(size(im), target_size, max_size);
@@ -13,10 +14,11 @@ function [im, im_scale] = prep_im_for_blob(im, im_means, target_size, max_size)
         target_size = round([size(im, 1), size(im, 2)] * im_scale);
         im = imresize(im, target_size, 'bilinear', 'antialiasing', false);
     else
-        % for im as gpuArray
+        % for im as gpuArray 可能是imresize函数的差异
         try
             im = bsxfun(@minus, im, im_means);
         catch
+            % im_means的最大缩放比例
             im_means_scale = max(double(size(im, 1)) / size(im_means, 1), double(size(im, 2)) / size(im_means, 2));
             im_means = imresize(im_means, im_means_scale);    
             y_start = floor((size(im_means, 1) - size(im, 1)) / 2) + 1;
