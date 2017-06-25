8654db0725931ab924a0c504e3ff05d582f10886
zavix <xzhewei@gmail.com>
2017-06-25 20:04:55 +0800

    start scut datasets experiments

---
+++ experiments/+Dataset/scut_test.m
@@ -0,0 +1,14 @@
+function dataset = scut_test(dataset, usage)
+
+switch usage
+    case {'train'}
+        dataset.imdb_train    = {  imdb_from_scut('./datasets/scut', 'test', true) };
+        dataset.roidb_train   = cellfun(@(x) x.roidb_func(x, true), dataset.imdb_train, 'UniformOutput', false);
+    case {'test'}
+        dataset.imdb_test     = imdb_from_scut('./datasets/scut', 'test', false) ;
+        dataset.roidb_test    = dataset.imdb_test.roidb_func(dataset.imdb_test, false);
+    otherwise
+        error('usage = ''train'' or ''test''');
+end
+
+end
